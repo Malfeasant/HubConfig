891a781ced964d55c1b63511140d6d3804c92d88
Mischa Sumrall <1212508+Malfeasant@users.noreply.github.com>
2025-08-04 08:18:36 -0700

    Basic appearance

--- src/main/java/us/malfeasant/templatefx/App.java
+++ src/main/java/us/malfeasant/templatefx/App.java
@@ -1,6 +1,12 @@
 package us.malfeasant.templatefx;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 /**
@@ -10,7 +16,18 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        stage.setTitle("Hello World!");
+        stage.setTitle("HubConfig");
+        var labelIp = new Label("IP Address of Hub:");
+        var fieldIp = new TextField();
+        var buttonConnect = new Button("Connect");
+        buttonConnect.setDefaultButton(true);
+
+        var grid = new GridPane();
+        grid.addRow(0, labelIp, fieldIp, buttonConnect);
+        grid.setHgap(10);
+        grid.setPadding(new Insets(10));
+        stage.setScene(new Scene(grid));
+
         stage.show();
     }
 
